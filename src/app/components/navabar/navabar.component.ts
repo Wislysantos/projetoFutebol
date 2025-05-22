2a38f0332eb94798d31166c84b7abfa0f07df4b0
Wislysantos <wislysantos@gmail.com>
2025-05-22 19:16:05 -0300

    add logo e os links do icon

--- src/app/components/navabar/navabar.component.ts
+++ src/app/components/navabar/navabar.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 import { ToolbarModule } from 'primeng/toolbar';
 import { ButtonModule } from 'primeng/button';
 
+import { ImageModule } from 'primeng/image';
+
 @Component({
   selector: 'app-navabar',
-  imports: [ToolbarModule, ButtonModule],
+  imports: [ToolbarModule, ButtonModule, ImageModule],
   templateUrl: './navabar.component.html',
   styleUrl: './navabar.component.css'
 })
